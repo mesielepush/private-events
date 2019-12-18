5863beac166207cbfb21915afb04cecadc30c270
mesielepush <48403308+mesielepush@users.noreply.github.com>
2019-12-18 15:21:00 -0600

    events model done

--- private-event/db/schema.rb
+++ private-event/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_194758) do
+ActiveRecord::Schema.define(version: 2019_12_18_211544) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.text "description"
+    t.datetime "date_event"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
