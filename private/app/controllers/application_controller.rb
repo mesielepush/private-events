08fe6527643b8c110742fec3333cabb280f48588
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-01-29 09:17:01 -0600

    controllers

--- private/app/controllers/application_controller.rb
+++ private/app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    helper_method :current_user?, :current_user
+    
+    def sign_in(user)
+        session[:user_id] = user.user_id
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def current_user?(user)
+        user == current_user
+    end
+    
+    def logged_in?
+        !current_user.nil?
+    end
+    
+    def sign_out
+        session.delete(:user_id)
+        @current_user = nil
+    end
+
 end
---
+++ private/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class UsersControllers < ApplicationController
+
+    def new
+        @user = User.new
+    end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            sign_in(@user)
+            flash[:succcess] = "This is Private Events Mr #{@user}"
+            render 'show'
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+
+end
