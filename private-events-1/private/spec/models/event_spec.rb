84b8f6fc7a7f790b227c31e1a5e904f3e5b5c4ce
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-02-05 13:28:43 -0600

    setting user_events rspec

--- private-events-1/private/spec/models/event_spec.rb
+++ private-events-1/private/spec/models/event_spec.rb
@@ -17,5 +17,14 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'creates several Events and tells you how many are upcoming and how many are past' do
+    user = User.create(name:  Faker::Name.first_name, email: Faker::Internet.email)
+    Event.create(name: 'Up 1', location: 'Somewhere', date: '2020/12/31', creator: user)
+    Event.create(name: 'Up 2', location: 'Elsewhere', date: '2021/12/31', creator: user)
+    Event.create(name: 'Pass 1', location: 'Yes sure', date: '2000/01/01', creator: user)
+    Event.create(name: 'Pass 2', location: 'Yes sure', date: '1984/01/01', creator: user)
+
+    expect(user.events.past.length).to eq(2)
+    expect(user.events.upcoming.length).to eq(2)
+  end
 end
--- private-events-1/private/spec/models/user_event_spec.rb
+++ private-events-1/private/spec/models/user_event_spec.rb
@@ -14,5 +14,16 @@
 require 'rails_helper'
 
 RSpec.describe UserEvent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  creator  = User.create(name:  Faker::Name.first_name, email: Faker::Internet.email)
+  attendee_1  = User.create(name:  Faker::Name.first_name, email: Faker::Internet.email)
+  attendee_2  = User.create(name:  Faker::Name.first_name, email: Faker::Internet.email)
+  first_event = Event.new(name: 'Up 1', location: 'Somewhere', date: '2020/12/31', creator: creator).save
+  second_event = Event.new(name: 'Up 1', location: 'Somewhere', date: '2021/12/31', creator: creator).save
+
+  it 'register some attendees to an event' do
+    first_register = UserEvent.new(user_id: attendee_1.user_id, event_id: first_event.creator_id).save
+    expect(first_register).to eql(true)
+    expect(attendee_1.attended_events.count).to eq(1)
+    expect(first_event.attendees.count).to eq(1)
+  end
 end
