de8685fa78b1621635504ae957e69b49f769bb42
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-02-06 23:13:51 -0600

    Update events_controller_spec.rb

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe EventsController, type: :controller do
     it 'returns http success' do
       creator = User.new(name: 'SomeUser', email: 'somemail@example.com')
       event1 = Event.new(name: 'Event 1', location: 'Somewhere', date: '2022/01/01',
-                        description: 'djahdkjahskdjhaskdjhas', creator: creator)
+                         description: 'djahdkjahskdjhaskdjhas', creator: creator)
       event1.save
       get :index, params: { id: event1.id }
       expect(response).to have_http_status(:success)
